7fda3dc7bb95fa149809873acc96752715fefa93
herno <russo.hernan@gmail.com>
2020-02-09 15:31:33 -0300

    Truncate card text and title

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,26 +6,30 @@ const Card = ({ title, description, lastUpdated, startDate, status }) => {
     return (
         <div className="Card">
             <div className="Card__header">
-                <div className="Card__header-left">
+                <div className="Card__header-top">
                     <h3>{title}</h3>
+                    <div className="Card__header-right">
+                        <div className="Card__status">
+                            <p>{status}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Card__meta-data">
                     <p>Start Date: {startDate}</p>
                     <p>Last Updated: {lastUpdated}</p>
                 </div>
-                <div className="Card__header-right">
-                    <div className="Card__status">
-                        <p>{status}</p>
-                    </div>
+            </div>
+            <div className="Card__content">
+                <div className="Card__description">
+                    <p>
+                        {description}
+                    </p>
                 </div>
+                <button className="Card__button">
+                    <p>More Icon</p>
+                    <p>More</p>
+                </button>
             </div>
-            <div className="Card__description">
-                <p>
-                    {description}
-                </p>
-            </div>
-            <button className="Card__button">
-                <p>More Icon</p>
-                <p>More</p>
-            </button>
         </div>
     );
 };
